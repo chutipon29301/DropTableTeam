a6d721edb35061009642aadab32de4f118d010f7
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-02-17 15:17:02 +0700

    Add lib

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var a = 0;
+var express = require('express');
+var app = express();
+
+app.set("view engine","pug");
+app.use(express.static('public'));
+app.listen(3000, _ => {
+    console.log('listening on port 3000');
+});
+
