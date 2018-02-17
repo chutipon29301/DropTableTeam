22be9f5acbcc1ec39763010546bb3c26cc6329fb
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-02-17 15:48:45 +0700

    Change port

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var app = express();
 
 app.set("view engine", "pug");
 app.use(express.static('public'));
-app.listen(80, _ => {
+app.listen(3000, _ => {
     console.log('listening on port 3000');
 });
 
 app.get('/', (req, res) => {
-    // res.status(200).send('OK');
     res.status(200).render('index');
 });
